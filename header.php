b0eb82a8bcc16bfb4169b3e9e148e22ac56b2411
Allison Tarr <hello@allisontarr.com>
2017-08-30 18:47:11 -0400

    do over

---
+++ header.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * The header for our theme.
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package atarr
+ */
+
+?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+	<?php global $is_IE; if ( $is_IE ) : ?>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+	<?php endif; ?>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<div id="page" class="site">
+	<a class="skip-link screen-reader-text" href="#main"><?php esc_html_e( 'Skip to content', 'atarr' ); ?></a>
+
+	<header class="site-header">
+		<div class="wrap">
+
+		<h1 class="site-title"><a class="home-link" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php esc_html_e( 'Allison Tarr', 'atarr' ); ?></a></h1>
+
+		<button class="sliding-panel-button mobile-menu-toggle" type="button">
+			<span class="mobile-menu-bar line-1"></span>
+			<span class="mobile-menu-bar line-2"></span>
+			<span class="mobile-menu-bar line-3"></span>
+			<span class="screen-reader-text"><?php esc_html_e( 'Menu', 'rcs' ); ?></span>
+		</button>
+
+		<div class="navigation-wrapper sliding-panel-content header-mobile">
+
+			<nav id="site-navigation" class="main-navigation">
+				<?php
+					wp_nav_menu( array(
+						'theme_location' => 'primary',
+						'menu_id'        => 'primary-menu',
+						'menu_class'     => 'menu menu-horizontal main-menu',
+					) );
+				?>
+			</nav><!-- #site-navigation -->
+		</div>
+
+		</div>
+	</header><!-- #masthead -->
+
+	<div id="content" class="site-content">
---
+++ inc/scripts.php
@@ -0,0 +1,101 @@
+<?php
+/**
+ * Custom scripts and styles.
+ *
+ * @package atarr
+ */
+
+/**
+ * Register Google font.
+ *
+ * @link http://themeshaper.com/2014/08/13/how-to-add-google-fonts-to-wordpress-themes/
+ */
+function atarr_font_url() {
+
+	$fonts_url = '';
+
+	/**
+	 * Translators: If there are characters in your language that are not
+	 * supported by the following, translate this to 'off'. Do not translate
+	 * into your own language.
+	 */
+	$garamond = _x( 'on', 'EB Garamond font: on or off', 'atarr' );
+	$libre_franklin = _x( 'on', 'Libre Franklin font: on or off', 'atarr' );
+
+	if ( 'off' !== $garamond || 'off' !== $libre_franklin ) {
+		$font_families = array();
+
+		if ( 'off' !== $garamond ) {
+			$font_families[] = 'EB Garamond';
+		}
+
+		if ( 'off' !== $libre_franklin ) {
+			$font_families[] = 'Libre Franklin:400,700';
+		}
+		// https://fonts.googleapis.com/css?family=EB+Garamond|Libre+Franklin:400,700.
+		$query_args = array(
+			'family' => urlencode( implode( '|', $font_families ) ),
+		);
+
+		$fonts_url = add_query_arg( $query_args, '//fonts.googleapis.com/css' );
+	}
+
+	return $fonts_url;
+}
+
+/**
+ * Enqueue scripts and styles.
+ */
+function atarr_scripts() {
+	/**
+	 * If WP is in script debug, or we pass ?script_debug in a URL - set debug to true.
+	 */
+	$debug = ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG == true ) || ( isset( $_GET['script_debug'] ) ) ? true : false;
+
+	/**
+	 * If we are debugging the site, use a unique version every page load so as to ensure no cache issues.
+	 */
+	$version = '1.0.0';
+
+	/**
+	 * Should we load minified files?
+	 */
+	$suffix = ( true === $debug ) ? '' : '.min';
+
+	// Register styles.
+	wp_register_style( 'atarr-google-font', atarr_font_url(), array(), null );
+
+	// Enqueue styles.
+	wp_enqueue_style( 'atarr-google-font' );
+	wp_enqueue_style( 'atarr-style', get_stylesheet_directory_uri() . '/style' . $suffix . '.css', array(), $version );
+
+	// Enqueue scripts.
+	wp_enqueue_script( 'atarr-scripts', get_template_directory_uri() . '/assets/js/project' . $suffix . '.js', array( 'jquery' ), $version, true );
+
+	// Enqueue p5.js.
+	wp_enqueue_script( 'atarr-p5', get_template_directory_uri() . '/assets/js/p5' . $suffix . '.js', array( 'jquery' ), $version, true );
+	wp_enqueue_script( 'atarr-p5-dom', get_template_directory_uri() . '/assets/js/p5.dom' . $suffix . '.js', array( 'jquery' ), $version, true );
+	wp_enqueue_script( 'orbs', get_template_directory_uri() . '/assets/js/concat/orbs.js', array( 'jquery', 'atarr-p5' ), $version, true );
+
+	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+		wp_enqueue_script( 'comment-reply' );
+	}
+
+	// Enqueue the mobile nav script.
+	wp_enqueue_script( 'atarr-mobile-nav', get_template_directory_uri() . '/assets/js/mobile-nav-menu.js', array( 'jquery' ), $version, true );
+}
+add_action( 'wp_enqueue_scripts', 'atarr_scripts' );
+
+/**
+ * Add SVG definitions to <head>.
+ */
+function atarr_include_svg_icons() {
+
+	// Define SVG sprite file.
+	$svg_icons = get_template_directory() . '/assets/images/svg-icons.svg';
+
+	// If it exsists, include it.
+	if ( file_exists( $svg_icons ) ) {
+		require_once( $svg_icons );
+	}
+}
