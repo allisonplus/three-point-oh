3c4d9ae3fe8be127f37f5e635c65c0c909448818
Allison Tarr <hello@allisontarr.com>
2017-09-03 16:46:44 -0400

    adjust conditional

--- template-parts/content.php
+++ template-parts/content.php
@@ -14,8 +14,8 @@
 		<?php
 		if ( is_single() ) {
 			the_title( '<h1 class="entry-title">', '</h1>' );
-		} else {
-			// the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+		} elseif ( ! is_front_page() ) {
+			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
 		}
 
 		if ( 'post' === get_post_type() ) : ?>
