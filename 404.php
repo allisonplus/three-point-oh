1b1764417ab52a65dc6f07ac05564406db1dd10a
Allison Tarr <hello@allisontarr.com>
2017-09-10 23:39:21 -0400

    404-ing + search resullts

--- 404.php
+++ 404.php
@@ -24,36 +24,37 @@ get_header(); ?>
 						<?php
 							get_search_form();
 
-							the_widget( 'WP_Widget_Recent_Posts' );
-
 						// Only show the widget if site has multiple categories.
 						if ( atarr_categorized_blog() ) :
 						?>
 
-						<div class="widget widget_categories">
-							<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'atarr' ); ?></h2>
-							<ul>
-							<?php
-								wp_list_categories( array(
-									'orderby'    => 'count',
-									'order'      => 'DESC',
-									'show_count' => 1,
-									'title_li'   => '',
-									'number'     => 10,
-								) );
-							?>
-							</ul>
-						</div><!-- .widget -->
+						<div class="widget-shell">
 
-						<?php
-							endif;
+							<?php the_widget( 'WP_Widget_Recent_Posts' );  ?>
 
-							/* translators: %1$s: smiley */
-							$archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'atarr' ), convert_smilies( ':)' ) ) . '</p>';
-							the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
+							<div class="widget widget_categories">
+								<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'atarr' ); ?></h2>
+								<ul>
+								<?php
+									wp_list_categories( array(
+										'orderby'    => 'count',
+										'order'      => 'DESC',
+										'show_count' => 1,
+										'title_li'   => '',
+										'number'     => 10,
+									) );
+								?>
+								</ul>
+							</div><!-- .widget -->
 
-							the_widget( 'WP_Widget_Tag_Cloud' );
-						?>
+					<?php
+						endif;
+
+						/* translators: %1$s: smiley */
+						$archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'atarr' ), convert_smilies( ':)' ) ) . '</p>';
+						the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
+					?>
+						</div>
 
 					</div><!-- .page-content -->
 				</section><!-- .error-404 -->
