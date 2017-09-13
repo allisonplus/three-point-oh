e81dd6cea07e2b97624994e9baac584e36edadcb
Allison Tarr <hello@allisontarr.com>
2017-09-13 10:47:48 -0400

    add/uncomment analytics + fix margin collapse

--- inc/scripts.php
+++ inc/scripts.php
@@ -131,3 +131,22 @@ function atarr_include_svg_icons() {
 		require_once( $svg_icons );
 	}
 }
+
+add_action( 'wp_footer', 'atarr_google_analytics' );
+/**
+ * Add Google Analytics Tracking.
+ */
+function atarr_google_analytics() {
+	// Hook into the footer. ?>
+	<script>
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+		ga('create', 'UA-60848717-1', 'auto');
+		ga('send', 'pageview');
+	</script>
+
+	<?php
+}
